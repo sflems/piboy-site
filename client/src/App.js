461ed613937867a49144dbb872686d0619d39a0d
sflems <flems17@gmail.com>
2021-05-27 08:24:15 -0700

    Reinstall client

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { getSharedData } from "./utils";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [sharedData, setSharedData] = useState({ isLoaded: false });
+
+    useEffect(() => {
+        getSharedData().then((data) => {
+            console.log(data);
+            setSharedData({ ...data, isLoaded: true });
+        });
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Hello!</p>
+                <Firstname {...sharedData} />
+                <Message {...sharedData} />
+            </header>
+        </div>
+    );
 }
+
+const Firstname = ({ first_name }) => {
+    if (!first_name) return <div>Loading ....</div>;
+
+    return <div>I am {first_name}</div>;
+};
+
+const Message = ({ isLoaded, message }) => {
+    if (!isLoaded) return null;
+    return <div>{message}</div>;
+};
 
 export default App;
