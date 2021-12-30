876aa57623b84eb32cdbb3f487d39f4714869b5e
sflems <scott@piboy.ca>
2021-12-29 16:48:53 -0800

    Previewer ready, TODO: conversion

--- client/src/components/fcc/MarkdownPreviewer.jsx
+++ client/src/components/fcc/MarkdownPreviewer.jsx
@@ -1,9 +1,18 @@
 import React, { useState } from "react";
 import { variants } from "../../Constants";
 import { motion } from "framer-motion";
+import "./MarkdownPreviewer.css";
 
 export default function MarkdownPreviewer() {
-  const [ inputText, setInputText ] = useState(null);
+  const [inputText, setInputText] = useState(null);
+  const [ouput, setOutput] = useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputText);
+    // Convert inputText to markdown here.
+
+  };
 
   return (
     <motion.div
@@ -16,8 +25,17 @@ export default function MarkdownPreviewer() {
         className="col p-3 my-auto text-center lead"
         variants={variants.container}
       >
-        Lead Text
+        Enter markdown text to be converted:
       </motion.p>
+      <div
+        className="input-group mb-4"
+        type="textarea"
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+      >
+        <textarea id="editor" className="form-control input" aria-label="Markdown text entry area"/>
+      </div>
+
       <div className="d-grid gap-2 d-md-flex pb-2 justify-content-center">
         <div>
           <motion.button
@@ -29,12 +47,19 @@ export default function MarkdownPreviewer() {
             type="button"
             className="btn btn-info text-white btn-lg px-4 rounded-0 shadow"
             id="new-quote"
-            onClick={() => null}
+            onClick={handleSubmit}
           >
             Preview
           </motion.button>
         </div>
       </div>
+      <motion.pre
+        id="preview"
+        className="col p-3 my-auto text-left"
+        variants={variants.container}
+      >
+        {inputText && inputText}
+      </motion.pre>
     </motion.div>
   );
 }
